e727f27e10cadffd2b80df57d1da608fd90b9ee5
Mukhopadhyay <praneshmukherjee7@gmail.com>
2022-04-06 03:15:49 +0530

    Removing test.md

--- gen_docs.py
+++ gen_docs.py
@@ -22,27 +22,12 @@ for dir, mapping in directories:
         for i in range(0, len(ims), 2):
             try:
                 im1, im2 = ims[i], ims[i+1]
-                # strings.append(f'|_{im1}_|_{im2}_|')
-                # strings.append( '|:------|:------|')
                 string = f'<img src="../gallery/Histogram/{im1}" width="45%"> <img src="../gallery/Histogram/{im2}" width="45%">'
             except IndexError:
                 im1 = ims[i]
                 string = f'<img src="../gallery/Histogram/{im1}" width="45%">'
             finally:
                 strings.append(string+'\n')
-
-
-    # for i in range(0, len(images), 2):
-    #     try:
-    #         im1, im2 = images[i], images[i+1]
-    #         # strings.append(f'|_{im1}_|_{im2}_|')
-    #         # strings.append( '|:------|:------|')
-    #         string = f'<img src="gallery/Histogram/{im1}" width="40%"> <img src="gallery/Histogram/{im2}" width="40%">'
-    #     except IndexError:
-    #         im1 = images[i]
-    #         string = f'<img src="gallery/Histogram/{im1}">'
-    #     finally:
-    #         strings.append(string+'\n')
 
     fname = os.path.join(dir, 'README.md')
     with open(fname, 'w') as file:
